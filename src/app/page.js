2226c16c30bbab74b55f6869a18eba56f244b2ee
Noel Miranda <noel_miranda@epam.com>
2024-07-17 13:28:11 +0200

    test 1.0

--- src/app/page.js
+++ src/app/page.js
@@ -54,6 +54,9 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Find in-depth information about Next.js features and API.
+            Find in-depth information about Next.js features and API.
+            Find in-depth information about Next.js features and API.
+            Find in-depth information about Next.js features and API.
           </p>
         </a>
 
@@ -71,6 +74,9 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
 
@@ -87,6 +93,11 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore starter templates for Next.js.
+            Explore starter templates for Next.js.
+            Explore starter templates for Next.js.
+            Explore starter templates for Next.js.
+            Explore starter templates for Next.js.
             Explore starter templates for Next.js.
           </p>
         </a>
